6667c589f15ed49c8e2643086421a4eeb9732fd2
Otiro-John <126407500+Otiro-John@users.noreply.github.com>
2024-03-17 13:19:10 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,63 @@
+(function ($) {
+    "use strict";
+ 
+ // vegetable carousel
+ $(".vegetable-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: true,
+    loop: true,
+    margin: 25,
+    nav : true,
+    navText : [
+        '<i class="bi bi-arrow-left text-danger"></i>',
+        '<i class="bi bi-arrow-right text-danger"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+   // Product Quantity
+   $('.quantity button').on('click', function () {
+    var button = $(this);
+    var oldValue = button.parent().parent().find('input').val();
+    if (button.hasClass('btn-plus')) {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    button.parent().parent().find('input').val(newVal);
+});
+
+})(jQuery);
